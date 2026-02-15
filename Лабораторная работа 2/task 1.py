309b25b4a1e65a863183699dc769d5b956281ee2
Alisa <Alisa.barabanova03@mail.ru>
2026-02-16 02:15:48 +0300

    LR-2

---
+++ Лабораторная работа 2/task 1.py
@@ -0,0 +1,39 @@
+BOOKS_DATABASE = [
+    {"id": 1, "name": "test_name_1", "pages": 200},
+    {"id": 2, "name": "test_name_2", "pages": 400},
+]
+
+
+class Book:
+    def __init__(self, id: int, name: str, pages: int):
+        self.id = id
+        self.name = name
+        self.pages = pages
+
+    def __str__(self) -> str:
+        return f'Книга "{self.name}"'
+
+    def __repr__(self) -> str:
+        return f"Book(id_={self.id}, name='{self.name}', pages={self.pages})"
+
+
+books = [Book(**book) for book in BOOKS_DATABASE]
+
+for book in books:
+    print(book)
+
+print(books)
+
+# TODO написать класс Book
+class Book:
+    def __init__(self, id: int, name: str, pages: int):
+        self.id = id
+        self.name = name
+        self.pages = pages
+
+    def __str__(self) -> str:
+        return f'Книга "{self.name}"'
+
+    def __repr__(self) -> str:
+        return f"Book(id={self.id}, name='{self.name}', pages={self.pages})"
+
